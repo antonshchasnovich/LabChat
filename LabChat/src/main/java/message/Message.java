744e698192235cc41aee7e7adfaa6bbd752f13b7
Anton Shchesnovich <sitcomsfan@yandex.by>
2018-12-13 09:25:52 +0300

    Split project on two parts, Console Client separately now

--- LabChat/src/main/java/message/Message.java
+++ LabChat/src/main/java/message/Message.java
@@ -3,6 +3,8 @@ package message;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @Builder
 public class Message {
@@ -27,5 +29,21 @@ public class Message {
 
     public MessageType getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(name, message.name) &&
+                Objects.equals(text, message.text) &&
+                type == message.type;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, text, type);
     }
 }
