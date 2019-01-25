96cc49f87e6af8239545681f3d7a04c8003a7b7b
Anton Shchesnovich <sitcomsfan@yandex.by>
2019-01-25 12:33:11 +0300

    add class Chat

---
+++ LabChat/src/main/java/user/chat/Chat.java
@@ -0,0 +1,38 @@
+package user.chat;
+
+import user.Agent;
+import user.Client;
+import util.IdGenerator;
+
+public class Chat {
+    private int id;
+    private Agent agent;
+    private Client client;
+
+    public Chat(Agent agent, Client client) {
+        id = IdGenerator.getChatId();
+        this.agent = agent;
+        this.client = client;
+    }
+
+    @Override
+    public String toString() {
+        return "Chat{" +
+                "id=" + id +
+                ", agent=" + agent +
+                ", client=" + client +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+}
