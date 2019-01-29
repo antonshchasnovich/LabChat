631dfec638276e72f00cec4ef4c60c2408433b51
Anton Shchesnovich <sitcomsfan@yandex.by>
2019-01-29 08:56:39 +0300

    add request agents with params

--- LabChat/src/main/java/RestAPI/ChatRestController.java
+++ LabChat/src/main/java/RestAPI/ChatRestController.java
@@ -13,10 +13,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import user.Agent;
 import user.Client;
 import user.SessionsStorage;
+import user.User;
 import user.chat.Chat;
 import user.httpUsers.HttpAgent;
 import user.httpUsers.HttpClient;
@@ -35,20 +37,39 @@ public class ChatRestController {
 
     private static SessionsStorage storage = SessionsStorage.getInstance();
 
-    @RequestMapping(value = "/allAgents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ArrayList<DTSUser>> getAllAgents(ModelMap map){
+    @RequestMapping(value = "/agents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ArrayList<DTSUser>> getAllAgents(ModelMap map) {
         ArrayList<DTSUser> result = new ArrayList<>();
-        for (Agent agent:storage.getAllAgents().values()
-             ) {
+        for (Agent agent : storage.getAllAgents().values()
+        ) {
             result.add(new DTSUser(agent));
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/freeAgents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ArrayList<DTSUser>> getFreeAgents(ModelMap map){
+    @RequestMapping(value = "/agents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ArrayList<DTSUser>> getAgents(@RequestParam("pageNumber") int pageNumber, @RequestParam("pageSize") int pageSize, ModelMap map) {
+        if (storage.getAllAgents().size() <= pageNumber * pageSize) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        ArrayList<User> agents = new ArrayList<>(storage.getAllAgents().values());
         ArrayList<DTSUser> result = new ArrayList<>();
-        for (Agent agent:storage.getFreeAgents()
+        if (storage.getAllAgents().size() <= (pageNumber + 1) * pageSize) {
+            for (User agent : agents.subList(pageNumber * pageSize, agents.size())
+            ) {
+                result.add(new DTSUser(agent));
+            }
+        } else {
+            for (User agent : agents.subList(pageNumber * pageSize, (pageNumber + 1) * pageSize)
+            ) {
+                result.add(new DTSUser(agent));
+            }
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/freeAgents", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ArrayList<DTSUser>> getFreeAgents(ModelMap map) {
+        ArrayList<DTSUser> result = new ArrayList<>();
+        for (Agent agent : storage.getFreeAgents()
         ) {
             result.add(new DTSUser(agent));
         }
@@ -56,9 +77,9 @@ public class ChatRestController {
     }
 
     @RequestMapping(value = "/waitingClients", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ArrayList<DTSUser>> getWaitingClients(ModelMap map){
+    public ResponseEntity<ArrayList<DTSUser>> getWaitingClients(ModelMap map) {
         ArrayList<DTSUser> result = new ArrayList<>();
-        for (Client client:storage.getWaitingClients()
+        for (Client client : storage.getWaitingClients()
         ) {
             result.add(new DTSUser(client));
         }
@@ -66,47 +87,46 @@ public class ChatRestController {
     }
 
     @RequestMapping(value = "/infoAboutAgent/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AllInfoAboutUser> getInfoAboutAgent(@PathVariable("id") long id, ModelMap map){
+    public ResponseEntity<AllInfoAboutUser> getInfoAboutAgent(@PathVariable("id") long id, ModelMap map) {
         Agent agent = storage.getAllAgents().get(id);
-        if (agent == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (agent == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(new AllInfoAboutUser(agent), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/infoAboutClient/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AllInfoAboutUser> getInfoAboutClient(@PathVariable("id") long id, ModelMap map){
+    public ResponseEntity<AllInfoAboutUser> getInfoAboutClient(@PathVariable("id") long id, ModelMap map) {
         Client client = storage.getAllClients().get(id);
-        if (client == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (client == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(new AllInfoAboutUser(client), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/freeAgentsCount", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Integer> getFreeAgentsCount(ModelMap map){
+    public ResponseEntity<Integer> getFreeAgentsCount(ModelMap map) {
         return new ResponseEntity<>(storage.getFreeAgents().size(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/allChats", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ArrayList<DTSChat>> getAllChats(ModelMap map){
+    @RequestMapping(value = "/chats", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ArrayList<DTSChat>> getAllChats(ModelMap map) {
         ArrayList<DTSChat> result = new ArrayList<>();
-        for (Chat chat:storage.getAllChats().values()
-             ) {
+        for (Chat chat : storage.getAllChats().values()
+        ) {
             result.add(new DTSChat(chat));
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/infoAboutChat/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AllInfoAboutChat> getInfoAboutChat(@PathVariable("id") long id, ModelMap map){
+    public ResponseEntity<AllInfoAboutChat> getInfoAboutChat(@PathVariable("id") long id, ModelMap map) {
         Chat chat = storage.getAllChats().get(id);
-        if (chat == null)return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (chat == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(new AllInfoAboutChat(chat), HttpStatus.OK);
     }
 
 
-
-    @RequestMapping(value = "/registerAgent/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity regAgent(@PathVariable("name") String name, HttpServletRequest request){
+    @RequestMapping(value = "/registerAgent/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity regAgent(@PathVariable("name") String name, HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         HttpAgent agent = new HttpAgent(request.getSession(), name);
         try {
             storage.regAgent(agent);
@@ -117,10 +137,10 @@ public class ChatRestController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/registerClient/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity regClient(@PathVariable("name") String name, HttpServletRequest request){
+    @RequestMapping(value = "/registerClient/{name}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity regClient(@PathVariable("name") String name, HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         HttpClient client = new HttpClient(request.getSession(), name);
         try {
             storage.regClient(client);
@@ -131,10 +151,10 @@ public class ChatRestController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/sendMessage/{text}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity sendMessage(@PathVariable("text") String text, HttpServletRequest request){
+    @RequestMapping(value = "/sendMessage/{text}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity sendMessage(@PathVariable("text") String text, HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(!storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (!storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         Message message = new Message(name, text, MessageType.TEXT_MESSAGE);
         try {
             storage.sendMessage(session, message);
@@ -145,17 +165,17 @@ public class ChatRestController {
     }
 
     @RequestMapping(value = "/getMessages", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getMessages(HttpServletRequest request){
+    public ResponseEntity getMessages(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(!storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (!storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         HttpUser httpUser = (HttpUser) storage.getAllUsers().get(session);
         return new ResponseEntity(httpUser.getMessages(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/leave", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity leaveChat(HttpServletRequest request){
+    @RequestMapping(value = "/leave", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity leaveChat(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(!storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (!storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         try {
             storage.leaveChat(session, 0);
         } catch (IOException | EncodeException e) {
@@ -164,10 +184,10 @@ public class ChatRestController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/exit", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity exitChat(HttpServletRequest request){
+    @RequestMapping(value = "/exit", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity exitChat(HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if(!storage.getAllUsers().keySet().contains(session))return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (!storage.getAllUsers().keySet().contains(session)) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         try {
             storage.exitChat(session);
         } catch (IOException | EncodeException e) {
